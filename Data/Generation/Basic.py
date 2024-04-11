dc1c34d40075841065d648a9ac6a309202a55c9d
Mark <doylethecat@gmail.com>
2024-04-11 16:53:50 -0400

    Basic Harness
    Created some basic generation, and set up the general relationship I hope to see between python files!

--- Data/Generation/Basic.py
+++ Data/Generation/Basic.py
@@ -1,11 +1,36 @@
 # Basic data creation methods!
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 
 def create_file(dir):
     x = open(dir, 'x+')
 
-def make_random(num_d=2, n_samples=100):
-    x = pd.DataFrame(np.random.randn(100, 2), columns=['x', 'y'])
+def make_random(start=0, stop=100, precision=1):
+    n = int(1 / precision * 100)
+    x = pd.DataFrame(np.random.randint(low=start, high=stop, size=(n,2)), columns=['x', 'y'])
     return x
+
+def make_linear(start=0, stop=100, precision=0.1):
+    x = np.arange(start, stop, precision)
+    y = np.arange(start, stop, precision)
+    df = pd.DataFrame()
+    df.insert(0, column='x', value=x)
+    df.insert(1, column='y', value=y)
+    return df
+
+
+if __name__ == "__main__":
+    x = make_random()
+    y = make_linear()
+
+    sns.set_style("darkgrid")
+    # hue="size", style="type", linewidth=2,
+    sns.scatterplot(data=x, x="x", y="y", palette="flare")
+
+    y.to_csv('Data/Storage/test1.csv')
+    plt.show()
+
--- Models/Harness.py
+++ Models/Harness.py
@@ -1,10 +1,16 @@
 # This is the main entry point for our research!
 # Call from this file
 import Data.Generation.Basic as Basic
-
-import matplotlib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 if __name__ == "__main__":
-    print(f"hello world!")
-    x = Basic.make_linear()
+    x = pd.read_csv('Data/Storage/test1.csv')
+    sns.set_style("darkgrid")
+    # hue="size", style="type", linewidth=2,
+    sns.scatterplot(data=x, x="x", y="y", palette="flare")
+    plt.show()
+
