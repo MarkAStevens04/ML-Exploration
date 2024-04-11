9f56bffb0145a746085ba5fc8ec9959aa6e2acf6
Mark <doylethecat@gmail.com>
2024-04-11 19:11:16 -0400

    Added pretty graphs
    Added some pretty graphs

--- Explorations/Harness.py
+++ Explorations/Harness.py
@@ -1,7 +1,7 @@
 # This is a basic entry point for our research!
 # Explore relationship btwn mlp & lin-reg in joining 2 variables together
 import Data.Generation.Basic as Basic
-import Models.LinReg_Basic as LogReg
+import Models.LinReg_Basic as LinReg
 import Models.MLP_Basic as MLP
 
 import pandas as pd
@@ -49,9 +49,6 @@ if __name__ == "__main__":
     valid_size = 0.10
     test_index = df.shape[0] - int(df.shape[0] * test_size)
     valid_index = test_index - int(df.shape[0] * test_size)
-    print(test_index)
-    print(valid_index)
-
 
     X_train = df[:valid_index,:-1]
     t_train = df[:valid_index,-1:]
@@ -67,25 +64,35 @@ if __name__ == "__main__":
     sns.set_style("darkgrid")
     # MLP.compare_models(X_train, t_train, X_valid, t_valid)
     m1 = MLP.make_model(X_train=X_train,t_train=t_train)
-    pred = predict_training(m1, X_valid[:10])
+    m2 = LinReg.make_model(X_train=X_train,t_train=t_train)
+
+    predMLP = predict_training(m1, X_valid[:10])
+    predLin = predict_training(m2, X_valid[:10])
 
     print(m1.score(X_train, t_train))
     print(m1.score(X_valid, t_valid))
 
-    # diff = pred['prediction'] - pred['X_test1']
-    actual_pos = pred['X_test1'] - pred['X_test2']
-    # pred.insert(3, column='diff', value=diff)
-    # pred.insert(4, column='actual', value=t_valid[:10])
-    pred.insert(3, column='actual_pos', value=actual_pos)
+    print(m2.score(X_train, t_train))
+    print(m2.score(X_valid, t_valid))
 
-    # print(diff)
+    actual_pos = predMLP['X_test1'] - predMLP['X_test2']
+    predMLP.insert(3, column='actual_pos', value=actual_pos)
+
+    actual_pos = predLin['X_test1'] - predLin['X_test2']
+    predLin.insert(3, column='actual_pos', value=actual_pos)
 
 
+    f, ax = plt.subplots(1, 2, figsize=(12, 6))
 
-    f, ax = plt.subplots(figsize=(6, 6))
+    sns.scatterplot(data=dfO, x="x", y="y", marker="s", hue="x2", palette="ch:r=-.2,d=.3_r", ax=ax[1])
+    sns.lineplot(data=predMLP, x="actual_pos", y="prediction", marker="o", c='r', ax=ax[1])
 
-    sns.scatterplot(data=dfO, x=dfO['x'], y=dfO['y'], marker="s", hue=dfO['x2'], palette="ch:r=-.2,d=.3_r")
-    sns.lineplot(data=pred, x="actual_pos", y="prediction", marker="o", c='r')
+    sns.scatterplot(data=dfO, x="x", y="y", marker="s", hue="x2", palette="ch:r=-.2,d=.3_r", ax=ax[0])
+    sns.lineplot(data=predLin, x="actual_pos", y="prediction", marker="o", c='b', ax=ax[0])
+
+    ax[0].set(title="Linear Regression")
+    ax[1].set(title="MLP")
+
     # #
     # ax.legend(ncol=2, loc="lower right", frameon=True)
 
